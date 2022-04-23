8b76e7001c6f49b8870f37deff802808bb34d9df
vsnsabari <vsnsabari@gmail.com>
2022-04-23 13:23:05 +0530

    Replaced Server sent events with socket implementation - Fix WSS Connection

--- ui/app/src/components/comments/RetroComments.tsx
+++ ui/app/src/components/comments/RetroComments.tsx
@@ -56,7 +56,7 @@ const RetroComments: React.FC<Props> = ({ classes }) => {
     useEffect(() => {
         if (!isSubscribed) {
             client.configure({
-                brokerURL: 'ws://retrometer.azurewebsites.net/retro-updates',
+                brokerURL: 'wss://retrometer.azurewebsites.net/retro-updates',
                 onConnect: () => {
                     console.log("connected");
                     client.subscribe(`/topic/comment/${state.sessionId}`, eventMsg => {
